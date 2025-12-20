5a4c3d1397eac8f49ae9cda40848a576547b7636
Nethma Gunawardana <nipunigunawardana777@gmail.com>
2025-12-20 15:50:33 +0530

    new image is added

--- number_plate_yolo.py
+++ number_plate_yolo.py
@@ -1,49 +1,43 @@
 import cv2
 from ultralytics import YOLO
 import easyocr
+import re
 
-# Load trained model
-model = YOLO("models/number_plate_yolo.pt")
+model = YOLO(
+    r"C:\Users\hp\Desktop\Vehicle Speed Detection\fyp_research\models\number_plate_yolo.pt"
+)
 
-# OCR reader
 reader = easyocr.Reader(['en'])
 
-# Video path
-video_path = r"C:\Users\hp\Desktop\Vehicle Speed Detection\fyp_research\videos\speeding3.mp4"
-cap = cv2.VideoCapture(video_path)
+img_path = r"C:\Users\hp\Desktop\Vehicle Speed Detection\fyp_research\videos\speeding4.jpg"
+img = cv2.imread(img_path)
 
-if not cap.isOpened():
-    print("Error: Cannot open video")
-    exit()
+results = model(img, imgsz=640, conf=0.25)
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+for r in results:
+    for box in r.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        plate_img = img[y1:y2, x1:x2]
 
-    results = model(frame)
+        # ===== OCR IMPROVEMENT =====
+        plate_img = cv2.resize(plate_img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+        gray = cv2.cvtColor(plate_img, cv2.COLOR_BGR2GRAY)
+        gray = cv2.bilateralFilter(gray, 11, 17, 17)
+        _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
-    for r in results:
-        for box in r.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
+        ocr_result = reader.readtext(thresh, detail=0)
 
-            plate_img = frame[y1:y2, x1:x2]
+        text = ""
+        if ocr_result:
+            text = " ".join(ocr_result)
+            text = re.sub(r'[^A-Z0-9]', '', text)
 
-            # OCR
-            ocr_result = reader.readtext(plate_img, detail=0)
+        print("Detected Plate:", text)
 
-            if ocr_result:
-                text = ocr_result[0]
-                print("Detected Plate:", text)
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0,255,0), 2)
+        cv2.putText(img, text, (x1, y1 - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
 
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(frame, text, (x1, y1 - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-    cv2.imshow("Sri Lankan ANPR", frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
+cv2.imshow("Sri Lankan ANPR", img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
